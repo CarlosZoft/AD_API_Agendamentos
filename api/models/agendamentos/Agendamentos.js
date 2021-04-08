1fe8025ff4e9072effafadc8fb05ce9b8d9d68c2
Carlos <carlosoftwar@gmail.com>
2021-04-07 22:04:20 -0300

    Update : adicionando métodos CRUD na class

--- api/models/agendamentos/Agendamentos.js
+++ api/models/agendamentos/Agendamentos.js
@@ -1,7 +1,8 @@
 const tabela_agendamento = require('./TabelaAgendamento');
 
 class Agendamento {
-    constructor({id,nome_cliente,nome_servico,status, data_agendamento,data_criacao, data_atualizacao}) {
+    constructor({id,nome_cliente,nome_servico,status, data_agendamento,
+        data_criacao, data_atualizacao}) {
         this.id = id;
         this.nome_cliente = nome_cliente;
         this.nome_servico = nome_servico;
@@ -13,14 +14,26 @@ class Agendamento {
 
     async criar() {
         const result = await tabela_agendamento.adicionar({
-            nome_cliente = this.nome_cliente,
-            nome_servico = this.nome_servico ,
-            status = this.status, 
-            data_agendamento = this.data_agendamento
+            nome_cliente : this.nome_cliente,
+            nome_servico : this.nome_servico,
+            status : this.status, 
+            data_agendamento : this.data_agendamento
         });
         this.id = result.id;
         this.data_criacao = result.data_criacao;
         this.data_atualizacao = result.data_atualizacao;
+    }
+    async buscar() {
+        const result = await tabela_agendamento.buscarPorID(this.id);
+        this.nome_cliente = result.nome_cliente;
+        this.nome_servico = result.nome_servico;
+        this.status = result.status;
+        this.data_agendamento = result.data_agendamento;
+        this.data_criacao = result.data_criacao;
+        this.data_atualizacao = result.data_atualizacao;
+    }
+    async excluir() {
+        await tabela_agendamento.remover(this.id);
     }
 }
 module.exports = Agendamento;
