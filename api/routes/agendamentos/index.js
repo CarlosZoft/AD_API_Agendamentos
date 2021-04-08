38e5eda1b6027215abf57b6a5e92fca8c5c208db
Carlos <carlosoftwar@gmail.com>
2021-04-07 22:05:10 -0300

    Update : adicionando rota delete

--- api/routes/agendamentos/index.js
+++ api/routes/agendamentos/index.js
@@ -1,16 +1,47 @@
 const router = require('express').Router()
 const TabelaAgendamento = require("../../models/agendamentos/TabelaAgendamento")
-const Agendamento = require("../../models/agendamentos/Agendamento")
+const Agendamento = require("../../models/agendamentos/Agendamentos")
 
-router.get('/', async (req, res) => {
+router.get('/', async (req , res) => {
     const results = await TabelaAgendamento.listar()
     res.send(JSON.stringify(results));
 })
 
-router.post('/', async (req,res) => {
+router.post('/', async (req , res) => {
     const reqAgendamento = req.body;
     const agendamento = new Agendamento(reqAgendamento);
     await agendamento.criar();
     res.send(JSON.stringify(agendamento))
+})
+router.get('/:id', async (req,res)=>{
+    try {
+        const id = req.params.id;
+        const agendamento = new Agendamento({id:id});
+        await agendamento.buscar();
+        res.send(JSON.stringify(agendamento));
+    }catch(error){
+        res.send(JSON.stringify({
+            mensage: error.mensage
+        }))
+    }
+})
+router.put('/', (req , res) =>{
+    
+})
+router.delete('/:id', async(req,res) => {
+    try{
+        const id = req.params.id;
+        const agendamento = new Agendamento({id:id});
+        await agendamento.excluir();
+        res.status(204).send(JSON.stringify(`Usuário de id : ${id} removido com sucesso!`));
+    }
+    catch(error){
+        res.send(JSON.stringify({
+            mensage: error.mensage
+        }))
+    }
+
+
+    
 })
 module.exports = router;
