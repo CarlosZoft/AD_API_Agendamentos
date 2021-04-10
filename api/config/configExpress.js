023b990979871e0eafe4f5f4adc0e455119ccb71
Carlos <carlosoftwar@gmail.com>
2021-04-09 22:08:24 -0300

    Add : Adicionando novo middleware

--- api/config/configExpress.js
+++ api/config/configExpress.js
@@ -1,10 +1,24 @@
 const express = require('express');
-const router = require('../routes/agendamentos')
+const router = require('../routes/agendamentos');
+const FormatoInvalido = require('../errors/FormatoInvalido');
+const FormatosValidos = require('../Serializar').FormatosValidos;
 
 
 module.exports = () => {
     const app = express();
 
+    app.use((req, res, next) => {
+        let formatoSolicitado = req.header('Accept');
+        if(formatoSolicitado === '*/*') {
+            formatoSolicitado = 'application/json'
+        }
+        if(FormatosValidos.indexOf(formatoSolicitado) === -1){
+            res.status(406);
+            return res.send();
+        }
+        res.setHeader('Content-Type', formatoSolicitado);
+        next();
+    })
     app.use(express.json());
     app.use('/api/agendamentos', router);
 
