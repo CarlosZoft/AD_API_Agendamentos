3691584d9d8e33484c5d127134ca413e192e0ee4
Carlos <carlosoftwar@gmail.com>
2021-04-14 01:20:31 -0300

    Add : adicionando model de tabela usuario

---
+++ api/models/usuarios/modelTabelaUsuario.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+const connection = require('../../db');
+
+
+const Usuario = connection.define("usuario",{
+
+    usuario :{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email :{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    senha :{
+        type: Sequelize.STRING,
+        allowNull: false
+        }
+    
+    },
+    {
+        freezeTableName: true,
+        tableName: 'usuario',
+        timestamps:true,
+        createAt: 'data_criacao',
+        updateAt: 'data_atualizacao'
+    }
+)
+module.exports = Usuario;
