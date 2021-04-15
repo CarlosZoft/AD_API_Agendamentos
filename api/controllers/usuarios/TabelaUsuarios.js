7d1b05b4859d9a12de50bbcbece5a19b1a3b5e6b
Carlos <carlosoftwar@gmail.com>
2021-04-14 23:11:58 -0300

    Add : tabela para controle de metodos sequelize

---
+++ api/controllers/usuarios/TabelaUsuarios.js
@@ -0,0 +1,76 @@
+const {listar}  = require('../Agendamentos/TabelaAgendamento')
+const modeloUsuario = require('../../models/usuarios/modelTabelaUsuario');
+
+
+module.exports = {
+
+    async listar() {
+        try{
+            return await modeloUsuario.findAll({
+                raw:true,
+            })
+        }
+        catch(error){
+            throw error;
+        }
+        
+    },
+
+    async adicionar(usuario){
+        try{
+            return await modeloUsuario.create(usuario);
+        }
+        catch(error){
+            throw error;
+        }
+    },
+
+    async atualizar(id,dados) {
+        try{
+            return await modeloUsuario.update(dados,
+                {
+                    where: {
+                        id:id
+                    }
+                })
+        }
+        catch(error){
+            throw error;
+        }
+    },
+
+    async remover(id){
+        try{
+            return await modeloUsuario.destroy({
+                where: {id : id}
+            });
+        }
+        catch(error){
+            throw error;
+        }
+       
+    },
+
+    async buscarPorID(id) {
+        try{
+           return await modeloUsuario.findByPk(id);    
+        }
+        catch(error){
+            throw error;
+        }
+    },  
+
+    async buscarPorEmail(email) {
+       try{ 
+             return await modeloUsuario.findOne({
+                where: {
+                    email:email
+                }
+            });    
+        }
+        catch(error){
+            throw error;
+        }
+    }
+
+}
