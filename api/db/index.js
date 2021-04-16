6ef77af6f782fa3e6ad5598055d955afd71c8daa
Carlos <carlosoftwar@gmail.com>
2021-04-15 23:41:15 -0300

    Update : trocando de .config para .env

--- api/db/index.js
+++ api/db/index.js
@@ -1,15 +1,16 @@
+require('dotenv').config({path:"../../env"})
 const Sequelize = require('sequelize');
-const config = require('config');
 
+console.log(process.env.mysql_password, "AQUI",process.env.mysql_database );
 const connection = new Sequelize(
-    config.get('mysql.database'),
-    config.get('mysql.user'),
-    config.get('mysql.password'),
-    {
-        host: config.get('mysql.host'),
-        dialect: 'mysql',
-        port: config.get('mysql.port')
-    }
+    process.env.MYSQL_DATABASE,
+    process.env.MYSQL_USER,
+    process.env.MYSQL_PASSWORD,
+     {
+         host:process.env.MYSQL_HOST,
+         dialect: 'mysql',
+         port:process.env.MYSQL_PORT
+     }
 )
 
 module.exports = connection;
